b6c22319f7693a08d43e4e2c215c91cde8d2bbe8
unknown <edjelvis@gmail.com>
2018-08-20 21:47:16 -0300

    Corrigindo layout da tela de cadastro de clinica médica

--- system/cadastro/cadastros.php
+++ system/cadastro/cadastros.php
@@ -29,49 +29,59 @@
       <div class="col-md-12" id="formulario_cadastro">
         <form class="form-horizontal" method="POST" action="../processa/proc_cad_usuario.php" id="form_centro_medico">
           <div class="form_section">
+            <h4 class="text-center mb1">Informações básicas</h4>
             <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
-              <div class="col-sm-10 col-md-12">
-                <input type="text" class="form-control" name="nome" placeholder="Nome Completo">
+              <div class="row">
+                <div class="col-sm-10 col-md-12">
+                  <label for="inputEmail3">Nome</label>
+                  <input type="text" class="form-control" name="nome" placeholder="Nome Completo">
+                </div>
               </div>
             </div>
 
             <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">CNPJ</label>
-              <div class="col-sm-10 col-md-12">
-                <input type="email" class="form-control" name="email" placeholder="CNPJ">
+              <div class="row">
+                <div class="col-sm-10 col-md-6">
+                  <label for="inputEmail3">CNPJ</label>
+                  <input type="email" class="form-control" name="email" placeholder="CNPJ">
+                </div>
+
+                <div class="col-sm-10 col-md-6">
+                  <label for="inputEmail3">Nome Fantasia</label>
+                  <input type="text" class="form-control" name="usuario" placeholder="Nome Fantasia">
+                </div>
               </div>
             </div>
 
-            <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">Nome Fantasia</label>
-              <div class="col-sm-10 col-md-12">
-                <input type="text" class="form-control" name="usuario" placeholder="Nome Fantasia">
-              </div>
-            </div>
           </div>
           <div class="form_section">
             <h4 class="text-center ma1">Endereço</h4>
             <div class="form-group">
-              <label for="inputPassword3" class="col-sm-2 col-md-2 control-label">CEP</label>
-              <div class="mb1 col-sm-10 col-md-12">
-                <input type="password" class="form-control" name="senha" placeholder="CEP">
+              <div class="row">
+                <label for="inputPassword3" class="col-sm-2 col-md-2 control-label">CEP</label>
+                <div class="mb1 col-sm-10 col-md-12">
+                  <input type="password" class="form-control" name="senha" placeholder="CEP">
+                </div>
               </div>
-              <label for="inputPassword3" class="col-sm-2 control-label">Rua</label>
-              <div class="mb1 col-sm-10 col-md-12">
-                <input type="password" class="form-control" name="senha" placeholder="Rua">
+              <div class="row">
+                <div class="mb1 col-sm-10 col-md-6">
+                  <label for="inputPassword3">Rua</label>
+                  <input type="password" class="form-control" name="senha" placeholder="Rua">
+                </div>
+                <div class="mb1 col-sm-10 col-md-6">
+                  <label for="inputPassword3">Número</label>
+                  <input type="password" class="form-control" name="senha" placeholder="Número">
+                </div>
               </div>
-              <label for="inputPassword3" class="col-sm-1 control-label">Número</label>
-              <div class="mb1 col-sm-10 col-md-12">
-                <input type="password" class="form-control" name="senha" placeholder="Número">
-              </div>
-              <label for="inputPassword3" class="col-sm-2 control-label">Bairro</label>
-              <div class="mb1 col-sm-10 col-md-12">
-                <input type="password" class="form-control" name="senha" placeholder="Bairro">
-              </div>
-              <label for="inputPassword3" class="col-sm-1 control-label">Cidade</label>
-              <div class="mb1 col-sm-10 col-md-12">
-                <input type="password" class="form-control" name="senha" placeholder="Cidade">
+              <div class="row">
+                <div class="mb1 col-sm-10 col-md-6">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Bairro</label>
+                  <input type="password" class="form-control" name="senha" placeholder="Bairro">
+                </div>
+                <div class="mb1 col-sm-10 col-md-6">
+                  <label for="inputPassword3" class="col-sm-1 control-label">Cidade</label>
+                  <input type="password" class="form-control" name="senha" placeholder="Cidade">
+                </div>
               </div>
             </div>
           </div>
@@ -88,35 +98,35 @@
           <div class="form_section pa1">
             <div class="form-group">
               <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
-              <div class="col-sm-10">
+              <div class="col-sm-12">
                 <input type="text" class="form-control" name="nome" placeholder="Nome Completo">
               </div>
             </div>
 
             <div class="form-group">
               <label for="inputEmail3" class="col-sm-2 control-label">E-mail</label>
-              <div class="col-sm-10">
+              <div class="col-sm-12">
                 <input type="email" class="form-control" name="email" placeholder="E-mail">
               </div>
             </div>
 
             <div class="form-group">
               <label for="inputEmail3" class="col-sm-2 control-label">Usuário</label>
-              <div class="col-sm-10">
+              <div class="col-sm-12">
                 <input type="text" class="form-control" name="usuario" placeholder="Usuário">
               </div>
             </div>
 
             <div class="form-group">
               <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
-              <div class="col-sm-10">
+              <div class="col-sm-12">
                 <input type="password" class="form-control" name="senha" placeholder="Senha">
               </div>
             </div>
 
             <div class="form-group">
               <label for="inputPassword3" class="col-sm-2 control-label">Nivel de Acesso</label>
-              <div class="col-sm-10">
+              <div class="col-sm-12">
                 <select class="form-control" name="nivel_de_acesso">
                   <option value="1">Administrativo</option>
                   <option value="2">Usuário</option>
@@ -135,28 +145,28 @@
         <form class="form-horizontal" method="POST" action="processa/proc_cad_usuario.php" id="form_especialidades">
           <div class="form-group">
             <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
-            <div class="col-sm-10">
+            <div class="col-sm-12">
               <input type="text" class="form-control" name="nome" placeholder="Nome Completo">
             </div>
           </div>
 
           <div class="form-group">
             <label for="inputEmail3" class="col-sm-2 control-label">E-mail</label>
-            <div class="col-sm-10">
+            <div class="col-sm-12">
               <input type="email" class="form-control" name="email" placeholder="E-mail">
             </div>
           </div>
 
           <div class="form-group">
             <label for="inputEmail3" class="col-sm-2 control-label">Usuário</label>
-            <div class="col-sm-10">
+            <div class="col-sm-12">
               <input type="text" class="form-control" name="usuario" placeholder="Usuário">
             </div>
           </div>
 
           <div class="form-group">
             <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
-            <div class="col-sm-10">
+            <div class="col-sm-12">
               <input type="password" class="form-control" name="senha" placeholder="Senha">
             </div>
           </div>
