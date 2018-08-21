08443f086ee4dbe6652a6adbe1427d4010b54f2e
unknown <edjelvis@gmail.com>
2018-08-20 22:24:33 -0300

    Corrigindo layout da tela de especialidade

--- system/cadastro/cadastros.php
+++ system/cadastro/cadastros.php
@@ -43,12 +43,12 @@
               <div class="row">
                 <div class="col-sm-10 col-md-6">
                   <label for="inputEmail3">CNPJ</label>
-                  <input type="email" class="form-control" name="email" placeholder="CNPJ">
+                  <input type="email" class="form-control" name="cpnj1" placeholder="CNPJ">
                 </div>
 
                 <div class="col-sm-10 col-md-6">
                   <label for="inputEmail3">Nome Fantasia</label>
-                  <input type="text" class="form-control" name="usuario" placeholder="Nome Fantasia">
+                  <input type="text" class="form-control" name="nome_fantasia" placeholder="Nome Fantasia">
                 </div>
               </div>
             </div>
@@ -60,7 +60,7 @@
               <div class="row">
                 <label for="inputPassword3" class="col-sm-2 col-md-2 control-label">CEP</label>
                 <div class="mb1 col-sm-10 col-md-12">
-                  <input type="password" class="form-control" name="senha" placeholder="CEP">
+                  <input type="text" class="form-control" name="cep" placeholder="CEP">
                 </div>
               </div>
               <div class="row">
@@ -136,7 +136,7 @@
 
           </div>
           <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10 to-right">
+            <div class="col-sm-12 to-right">
               <button type="submit" class="btn btn-success">Cadastrar</button>
             </div>
           </div>
@@ -145,45 +145,13 @@
         <form class="form-horizontal" method="POST" action="processa/proc_cad_usuario.php" id="form_especialidades">
           <div class="form_section pa1">
             <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">Nome</label>
+              <label for="inputEmail3" class="col-sm-2 control-label">Especialidade</label>
               <div class="col-sm-12">
-                <input type="text" class="form-control" name="nome" placeholder="Nome Completo">
+                <input type="text" class="form-control" name="especialidade" placeholder="Especialidade">
               </div>
             </div>
-
             <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">E-mail</label>
-              <div class="col-sm-12">
-                <input type="email" class="form-control" name="email" placeholder="E-mail">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">Usuário</label>
-              <div class="col-sm-12">
-                <input type="text" class="form-control" name="usuario" placeholder="Usuário">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
-              <div class="col-sm-12">
-                <input type="password" class="form-control" name="senha" placeholder="Senha">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="inputPassword3" class="col-sm-2 control-label">Nivel de Acesso</label>
-              <div class="col-sm-10">
-                <select class="form-control" name="nivel_de_acesso">
-                  <option value="1">Administrativo</option>
-                  <option value="2">Usuário</option>
-                </select>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <div class="col-sm-offset-2 col-sm-10">
+              <div class="col-sm-12 to-right">
                 <button type="submit" class="btn btn-success">Cadastrar</button>
               </div>
             </div>
